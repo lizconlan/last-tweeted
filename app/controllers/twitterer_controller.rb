c3966fe36ad7424962bc0f86074eb72441b7401b
lizconlan <lizconlan@gmail.com>
2009-04-10 00:57:45 +0100

    added placeholder index page

--- app/controllers/twitterer_controller.rb
+++ app/controllers/twitterer_controller.rb
@@ -1,5 +1,8 @@
 class TwittererController < ApplicationController
   
+  def index
+  end
+  
   def show
     screen_name = params[:screen_name]
     @twitterer = Twitterer.new(screen_name)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
+  map.root :controller => 'twitterer', :action => 'index'
   map.connect ':screen_name.:format', :controller => 'twitterer', :action => 'show'
   map.connect ':screen_name', :controller => 'twitterer', :action => 'show'
 end
