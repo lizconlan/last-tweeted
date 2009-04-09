ead32741c7213ce952188fd58b0b9fe687a5ecd3
lizconlan <lizconlan@gmail.com>
2009-04-09 22:04:55 +0100

    tidier model

--- app/models/twitterer.rb
+++ app/models/twitterer.rb
@@ -2,48 +2,42 @@ require 'hpricot'
 require 'open-uri'
 
 class Twitterer
-  class << self; 
-    attr_accessor :id
-    attr_accessor :name
-    attr_accessor :screen_name
-    attr_accessor :last_tweet_text
-    attr_accessor :last_tweeted 
-  end
-  
-  def id
-    @@id
-  end
-  
-  def name
-    @@name
-  end
-  
-  def screen_name
-    @@screen_name
-  end
-  
-  def last_tweet_text
-    @@last_tweet_text
-  end
-  
-  def last_tweeted
-    @@last_tweeted
-  end
-  
-  
+  attr_reader :id
+  attr_reader :name
+  attr_reader :screen_name
+  attr_reader :last_tweet_text
+  attr_reader :last_tweeted 
+    
   def initialize(screen_name)
-    @@screen_name = screen_name
+    @screen_name = screen_name
     
     doc = get_user_info(screen_name)
     
-    @@id = (doc/:user/:id).inner_html
-    @@name = (doc/:user/:name).inner_html
-    @@last_tweet_text = (doc/:user/:status/:text).inner_html
-    @@last_tweeted = (doc/:user/:status/:created_at).inner_html
+    if @error
+      @name = "ERROR: User Not Found"
+      @id = nil
+      @last_tweet_text = ""
+      @last_tweeted = nil
+    else
+      @id = (doc/:user/:id).inner_html
+      if (doc/:user/:protected).inner_html == "true"
+        @name = "This user has protected their updates"
+        @last_tweet_text = ""
+        @last_tweeted = nil
+      else
+        @name = (doc/:user/:name).inner_html
+        @last_tweet_text = (doc/:user/:status/:text).inner_html
+        @last_tweeted = (doc/:user/:status/:created_at).inner_html
+      end
+    end
   end
   
   def moribund?
     about_three_months = 60 * 60 * 24 * 30 * 3
+    
+    if last_tweeted.nil?
+      return nil
+    end
     
     if last_tweeted.blank?
       return true
@@ -58,11 +52,17 @@ class Twitterer
       end
     end
   end
+
+  def to_param
+    screen_name
+  end
   
   private
     def get_user_info(screen_name)
       doc = open("http://twitter.com/users/show/#{screen_name}.xml") do |f|
          Hpricot.XML(f)
       end
+      rescue
+        @error = true
     end
 end
